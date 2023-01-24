dcfd99c3d14e479d6353a837e36aa043e2d685d0
Ryder Rashid <ryderzane@Ryders-MacBook-Air-2.local>
2023-01-24 18:26:40 +0000

    fifth commit

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+
 public class BankAccountTest {
+    BankAccount bankAccount = new BankAccount(
+            "Ryder",
+            "Zane",
+            LocalDate.of(2001,01,05),
+            "234567",
+            0.05
+    );
+}
+
 }
